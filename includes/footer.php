9b91b4c6e1695f2a53c2760e968ae4087053b962
kaio Campos <kaiocamposti@gmail.com>
2019-10-18 00:09:20 -0300

    arquivos iniciais

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Loja Virtual</title>
+  </head>
+  <body>
